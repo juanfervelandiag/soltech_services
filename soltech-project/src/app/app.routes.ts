c9c66576f3443a0b50254190205bebfe5283dd46
lordonez <lizzysoft707@gmail.com>
2026-04-10 22:31:28 -0500

    router: registrar la ruta de administración en el sistema de navegación

--- soltech-project/src/app/app.routes.ts
+++ soltech-project/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Contact } from './pages/contact/contact';
 import { Favorites } from './pages/favorites/favorites';
+import { AdminComponent } from './pages/admin/admin.component';
 
 export const routes: Routes = [
     { path: '', component: Home },
     { path: 'favorites', component: Favorites },
-    { path: 'contact', component: Contact }
+    { path: 'contact', component: Contact },
+    { path: 'admin', component: AdminComponent }
 ];
