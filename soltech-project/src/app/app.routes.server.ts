9ad4b975d790f44b4fcfaa53568c0052d053c7e9
JuanFerVel <juanfervelandia14@gmail.com>
2026-04-12 22:41:00 -0500

    feat: Inicialización de la configuración del proyecto Angular y definición de las rutas de renderizado del lado del servidor

--- soltech-project/src/app/app.routes.server.ts
+++ soltech-project/src/app/app.routes.server.ts
@@ -2,6 +2,10 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'service/:id',
+    renderMode: RenderMode.Client
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
